1eb0625037ba60609fb86cd2d624b9e42eb43c73
JerryHe <mijerry1999@outlook.com>
2022-03-06 22:12:53 -0800

    token catched

--- src/components/SpotifyLoginLink.js
+++ src/components/SpotifyLoginLink.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-03-06 16:16:11
- * @LastEditTime: 2022-03-06 16:49:39
+ * @LastEditTime: 2022-03-06 20:41:14
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \cs499\song-guessr\src\components\SpotifyLoginLink.js
@@ -12,9 +12,9 @@ export default function SpotifyLoginLink() {
   const queryParams = new URLSearchParams({
     response_type: 'code',
     client_id: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
-    scope: "user-read-private user-read-email",
+    scope: "playlist-read-collaborative playlist-read-private user-read-email playlist-modify-public",
     redirect_uri: process.env.NEXT_PUBLIC_REDIRECT_URI
-  })
+  });
   const baseUrl = "https://accounts.spotify.com/authorize";
   const url = `${baseUrl}?${queryParams.toString()}`;
   return <a href={url}>Login with Spotify</a>
