d8e9ff691677252afcef7d528b79237ab2c41de8
jonah0502 <jonah0502@gmail.com>
2022-03-08 15:31:25 -0800

    header and footer made

---
+++ src/components/Layout.js
@@ -0,0 +1,35 @@
+import Head from 'next/head'
+import styles from '../styles/Layout.module.css'
+import Header from './Header'
+import Footer from './Footer'
+//import Showcase from './Showcase'
+import { useRouter } from 'next/router'
+
+
+export default function Layout({title, keywords, description, children}) {
+    
+    const router = useRouter()
+
+    return (
+        <div>
+            <Head>
+                <title>{title}</title>
+                <meta name='description' content = {description}/>
+                <meta name='keywords' content = {keywords}/>
+            </Head>
+            <Header/>
+            {/*router.pathname === '/' && <Showcase />*/}
+
+            <div className = {styles.container}> 
+                {children}
+            </div>
+            <Footer/>
+        </div>
+    )
+}
+
+Layout.defaultProps = {
+    title: 'DesignDen | Find the best designs',
+    description: 'Find sleek and modern website solutions',
+    keywords: 'Website, template, designden'
+}
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,54 +1,57 @@
-import React, { useState } from 'react';
-import { useRouter } from 'next/router';
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import Layout from "../components/Layout";
 
 function Login() {
   const router = useRouter();
-  const [ username, setUsername ] = useState("");
-  const [ password, setPassword ] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   async function handleLogin(e) {
     e.preventDefault();
     console.log("== Logging in with these credentials:", username, password);
-    const res = await fetch('/api/login', {
+    const res = await fetch("/api/login", {
       method: "POST",
       body: JSON.stringify({ username, password }),
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     });
     const resBody = await res.json();
     if (res.status !== 200) {
-      alert("Credentials invalid: " + resBody.err)
+      alert("Credentials invalid: " + resBody.err);
     } else {
       console.log("== resBody:", resBody);
       console.log("== document.cookie:", document.cookie);
       // window.localStorage.setItem('token', resBody.token)
-      router.push(router.query.redirect || '/');
+      router.push(router.query.redirect || "/");
     }
   }
 
   return (
-    <form onSubmit={handleLogin}>
-      <div>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={e => setUsername(e.target.value)}
-        />
-      </div>
-      <div>
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-      </div>
-      <div>
-        <button>Login</button>
-      </div>
-    </form>
+    <Layout title="About DesignDen">
+      <form onSubmit={handleLogin}>
+        <div>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <button>Login</button>
+        </div>
+      </form>
+    </Layout>
   );
 }
 
