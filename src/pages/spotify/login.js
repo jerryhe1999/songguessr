5f455916d09282f990b3be41b7334e055c9ac2b9
JerryHe <mijerry1999@outlook.com>
2022-03-13 13:11:00 -0700

    small edited

--- src/pages/spotify/login.js
+++ src/pages/spotify/login.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-03-06 15:29:58
- * @LastEditTime: 2022-03-13 12:57:42
+ * @LastEditTime: 2022-03-13 13:10:18
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \cs499\song-guessr\src\pages\github\login.js
@@ -52,6 +52,7 @@ export default function SpotifyLogin() {
   return (
     <div>
       {error && <p>Error: {error}</p>}
+      <h1>{user.display_name ? "Spotify logined": "Spotify login required"}</h1>
       <SpotifyLoginLink />
       {user.display_name && <p>Name: {user.display_name}</p>}
       {user.email && <p>Email: {user.email}</p>}
