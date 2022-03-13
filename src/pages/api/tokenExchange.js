28567181bad9500746778293f6523a26812971f0
JerryHe <mijerry1999@outlook.com>
2022-03-13 13:00:05 -0700

    having some problem with redis, directly use token at first

--- src/pages/api/tokenExchange.js
+++ src/pages/api/tokenExchange.js
@@ -1,25 +1,35 @@
 /*
  * @Author: your name
  * @Date: 2022-03-06 15:29:47
- * @LastEditTime: 2022-03-13 06:01:16
+ * @LastEditTime: 2022-03-13 12:16:10
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \cs499\song-guessr\src\pages\api\tokenExchange.js
  */
 import fetch from 'isomorphic-unfetch';
 import { setAuthCookie,generateAuthToken } from 'src/lib/auth';
+import { createClient } from 'redis';
 
 const client_id = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 const redirect_uri = process.env.NEXT_PUBLIC_REDIRECT_URI;
+//const redis_secret = process.env.REDIS_SECRET;
 
 const basic = Buffer.from(client_id+":"+client_secret, 'utf8').toString('base64');
+
+//const client = createClient();
+
+//client.connect();
+
+//client.on('connect', function() {
+//  console.log('Connected!');
+//});
 
 export default async (req, res) => {
   
   const { code } = req.body;
   if (!code) {
-    res.status(400).send({ err: "Must specify auth code" });
+    res.status(400).json({ err: "Must specify auth code" });
   }
   else {
       const spotifyRes = await fetch('https://accounts.spotify.com/api/token', {
@@ -27,7 +37,8 @@ export default async (req, res) => {
       body: new URLSearchParams({
         code: code,
         redirect_uri: redirect_uri,
-        grant_type: 'authorization_code'
+        grant_type: 'authorization_code',
+        scope: "user-read-email"
       }).toString(),
       headers: {
         Authorization: `Basic ${basic}`,
@@ -38,11 +49,13 @@ export default async (req, res) => {
     const spotifyResBody = await spotifyRes.json();
     if (spotifyResBody.access_token) {
       //store the access token in server side cache (eg. Redis)
+      const token = spotifyResBody.access_token;
+      //client.SETEX("token",600,token);
       setAuthCookie(res, generateAuthToken("..."));
-      res.status(200).send({ msg: "OK!" });
+      res.status(200).json({token: token});
     } 
     else {
-      res.status(401).send({ err: spotifyResBody.error_description });
+      res.status(401).json({err: spotifyResBody.error_description });
     }
   }
 }
--- src/pages/spotify/test_redis.js
+++ src/pages/spotify/test_redis.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-03-06 15:29:58
- * @LastEditTime: 2022-03-13 12:57:42
+ * @LastEditTime: 2022-03-13 10:00:04
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \cs499\song-guessr\src\pages\github\login.js
@@ -12,7 +12,6 @@ import { useRouter } from 'next/router';
 import SpotifyLoginLink from '../../components/SpotifyLoginLink'
 
 export default function SpotifyLogin() {
-  const [ user, setUser ] = useState({});
   const [ error, setError ] = useState("");
   const router = useRouter();
 
@@ -30,18 +29,7 @@ export default function SpotifyLogin() {
         setError(resBody.err)
       }
       else {
-        const { token } = resBody;
         console.log("== Auth succeeded!");
-        console.log(token);
-        const spotifyRes = await fetch('https://api.spotify.com/v1/me', {
-          method: 'GET',
-          headers: {
-              Authorization: "Bearer " + token
-          }
-      });
-      const spotifyResBody = await spotifyRes.json();
-      console.log(spotifyResBody);
-      setUser(spotifyResBody);
       }
     }
     if (router.query.code) {
@@ -53,8 +41,6 @@ export default function SpotifyLogin() {
     <div>
       {error && <p>Error: {error}</p>}
       <SpotifyLoginLink />
-      {user.display_name && <p>Name: {user.display_name}</p>}
-      {user.email && <p>Email: {user.email}</p>}
     </div>
   );
 }
